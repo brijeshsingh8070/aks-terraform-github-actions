0fbe32b16e7855131cfb9b279ba695e92393e03b
brijeshsingh8070 <singh.brijesh8070@gmail.com>
2026-08-09 19:10:58 +0530

    Associate NSG with Azure subnets

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -15,3 +15,19 @@ resource "azurerm_subnet" "this" {
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = each.value.address_prefixes
 }
+resource "azurerm_network_security_group" "this" {
+  for_each = var.subnets
+
+  name                = "${each.value.name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  for_each = var.subnets
+
+  subnet_id                 = azurerm_subnet.this[each.key].id
+  network_security_group_id = azurerm_network_security_group.this[each.key].id
+}
