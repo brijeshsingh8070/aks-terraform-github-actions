633bd4665fb0b00c3f521738826b2f0750e3eb75
brijeshsingh8070 <singh.brijesh8070@gmail.com>
2026-08-09 19:02:21 +0530

    Configure AKS API server authorized IP ranges

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,21 +19,21 @@ module "network" {
   tags = var.tags
 }
 module "aks" {
-
   source = "../../modules/aks"
 
   name                = var.aks_name
   location            = var.location
   resource_group_name = module.resource_group.resource_group_names["aks"]
 
-  subnet_id = module.network.subnet_ids["aks"]
-
   dns_prefix         = var.dns_prefix
   kubernetes_version = var.kubernetes_version
-  ssh_public_key     = var.ssh_public_key
+  subnet_id          = module.network.subnet_ids["aks"]
+
+  ssh_public_key = var.ssh_public_key
 
   node_pools = var.node_pools
 
+  api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
+
   tags = var.tags
 }
-
